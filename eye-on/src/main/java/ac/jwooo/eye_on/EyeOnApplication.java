42d97f07dd3918b3717a383a6512d4de45056a3c
sbmax002 <jwooooy002@gmail.com>
2026-04-11 20:52:37 +0900

    first commit

---
+++ eye-on/src/main/java/ac/jwooo/eye_on/EyeOnApplication.java
@@ -0,0 +1,13 @@
+package ac.example.eye.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeOnApplication.class, args);
+	}
+
+}
